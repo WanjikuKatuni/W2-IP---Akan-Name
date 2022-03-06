ab359c8e62fc95972285a3729ec8797f5e052c46
wanjiku <annritawanjiku77@gmail.com>
2022-03-07 02:30:55 +0300

    polish script file

--- JS/script.js
+++ JS/script.js
@@ -1,48 +1,61 @@
 //getting day of the week
-const getDay = function(date){
-    const parts = date.split("/")
+const getDay = function (date) {
+  const parts = date.split("/");
 
-    if(parts.length !=3){
-        alert("not a valid date")
-    }
-    var dt = new Date(date)
-    const dd = parts[0]
-    const mm = parts[1]
-    if(parts[2].length != 4 || mm < 1 || mm > 12){
-        alert("wrong date format")
-    }
+  if (parts.length != 3) {
+    alert("not a valid date");
+  }
+  var dt = new Date(date);
+  const dd = parts[0];
+  const mm = parts[1];
+  if (parts[2].length != 4 || mm < 1 || mm > 12) {
+    alert("wrong date format");
+  }
 
-    return dt.getDay()
-    }
+  return dt.getDay();
+};
 
+const logSubmit = function (event) {
+  //stopformsubmission
+  event.preventDefault();
 
-const logSubmit = function (event){
-    
-    //stopformsubmission
-    event.preventDefault()
+  const maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  const femaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
+  const formDate = document.getElementById("date").value;
+  const formGender = document.getElementById("gender").value;
 
-    const maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
-    const femaleNames = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-    const formDate = document.getElementById('date').value
-    const formGender = document.getElementById('gender').value
+  if (formDate === "" || formGender === "") {
+    alert("required fields are empty");
+  }
 
-    if(formDate === "" || formGender === "") {
-        alert("required fields are empty")
-    }
-    
-    const day = getDay(formDate)
+  const day = getDay(formDate);
 
-    var akanName = ""
+  var akanName = "";
 
-    if (formGender === 'M') {
-        akanName = maleNames[day]
-    }
-    else {
-        akanName = femaleNames[day]
-    }
-    alert('Your Akan Name is ' + akanName)
-}
+  if (formGender === "M") {
+    akanName = maleNames[day];
+  } else {
+    akanName = femaleNames[day];
+  }
+  alert("Your Akan Name is " + akanName);
+};
 
-const form = document.getElementById('akanform')
-const log = document.getElementById('log');
-form.addEventListener('submit', logSubmit)
+const form = document.getElementById("akanform");
+const log = document.getElementById("log");
+form.addEventListener("submit", logSubmit);
