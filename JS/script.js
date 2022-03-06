95d729b7bc87ccb9dfccaaaf74a37c3810f80d9d
wanjiku <annritawanjiku77@gmail.com>
2022-03-07 01:50:12 +0300

    add changes to script.js

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,6 @@
-const submit =function(){
-    const maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
-    const femaleNames = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-
-}
 //getting day of the week
 const getDay = function(date){
-    const parts = date.split('/')
+    const parts = date.toString().split("/")
 
     if(parts.length !=3){
         alert("not a valid date")
@@ -14,12 +9,39 @@ const getDay = function(date){
     const dd = parts[0]
     const mm = parts[1]
 
-    if(parts[2].length !=4){
-        alert("wrong date format")
-    }
+    console.log(parts[2])
+
+        if(parts[2].length !=4){
+            alert("wrong date format")
+        }
     const cc = parts[2][0] + parts[2][1]
     const yy = parts[2][2] + parts[2][3]
 
-    const d = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
-    return d
+        const d = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+        return d
+    }
+
+
+const submit =function(event){
+    // console.log("here")
+    //stopformsubmission
+    event.preventDefault()
+    const maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+    const femaleNames = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+    const formDate = document.getElementById('date').textContent
+    const formGender = document.getElementById('gender').value
+    
+    const day = getDay(formDate)
+    var akanName = ""
+
+    if (formGender === 'M') {
+        akanName = maleNames[day]
+    }
+    else {
+        akanName = femaleNames[day]
+    }
+    alert('Your Akan Name is ' + akanName)
 }
+
+// const form = document.querySelector('#akanform')
+// form.addEventListener('submit', submit(event))
